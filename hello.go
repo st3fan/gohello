1a0392acf236599f63163f0d6d0bbd0935b3df77
Stefan Arentz <stefan@arentz.ca>
2017-06-19 14:32:06 -0400

    Localization support

--- hello.go
+++ hello.go
@@ -1,11 +1,17 @@
 
 package hello
 
+import "fmt"
+
 // Hello returns a friendly hello message.
 
-func Hello(name string) string {
+func Hello(name string, locale string) (string, error) {
+	if locale != "en" {
+		return "", fmt.Errorf("unsupported locale: " + locale)
+	}
+
 	if name == "" {
 		name = "anonymous"
 	}
-	return "Hello, " + name + "!"
+	return "Hello, " + name + "!", nil
 }
